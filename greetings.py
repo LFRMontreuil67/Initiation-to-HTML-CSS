6498a65b24c9334837e9b3d1b0b1bd451ea81df9
LFRMontreuil67 <fruchard.luc@laposte.net>
2026-05-06 13:31:05 +0200

    Create greetings.py
    yep !

---
+++ greetings.py
@@ -0,0 +1,9 @@
+La commande :
+
+git pull origin main
+
+Cette commande signifie :
+
+git pull → Récupère les nouvelles modifications du repo distant.
+origin → Désigne l'emplacement distant du repo (appelé remote en Git, ici nommé origin).
+main → Cible la branche principale du projet.
